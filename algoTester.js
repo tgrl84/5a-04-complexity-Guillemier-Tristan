33dbf019367b23087f20108b4f2ce55dd4a1648d
Tristan Guillemier <71886156+tgrl84@users.noreply.github.com>
2025-02-10 12:45:16 +0100

    outils pour tester (pas terminé)

---
+++ algoTester.js
@@ -0,0 +1,15 @@
+class AlgoTester{
+    constructor(){
+        this.testSuit = [];
+        this.algorithmes = [];
+    }
+
+    addTestSuit(name,tests= []){
+        const suit = {nom : nom,tests :tests};
+        this.testSuit.push(suit);
+    }
+
+    addAlgo(algoName, algo) {
+        this.algorithmes[nomAlgo] = implementation;
+    }
+}
